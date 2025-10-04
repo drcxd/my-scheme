21503696e24b81f8a5004245e537d12177fb049d
Chang Xiaoduan <drcxd@sina.com>
2025-10-04 16:47:52 +0800

    Interactive mode and file mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
 
-int main(int argc, char **argv) {
+void runPrompt() {
   std::string line;
   while (true) {
     std::cout << ">>> ";
     std::getline(std::cin, line);
     std::cout << line << std::endl;
   }
-  return 0;
+}
+
+int runFile(std::string_view file) {
+  std::ifstream fs{file.data()};
+  if (fs.good()) {
+    auto size = std::filesystem::file_size(file);
+    std::string buffer(size, 0);
+    fs.read(buffer.data(), size);
+    std::cout << buffer;
+    return 0;
+  }
+  return -1;
+}
+
+int main(int argc, char **argv) {
+  int retCode = 0;
+  if (argc < 2) {
+    runPrompt();
+  } else {
+    std::string file{argv[1]};
+    retCode = runFile(file);
+  }
+  return retCode;
 }
