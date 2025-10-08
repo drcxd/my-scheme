e701211ad90f965f11be5b460a9117a11b10961c
Chang Xiaoduan <drcxd@sina.com>
2025-10-08 13:32:18 +0800

    Add a test for scanner

---
+++ tests/tokens.scm
@@ -0,0 +1,18 @@
+(a . b)
+
+'mischief ; comment
+;; comment as well
+#t #f
+     
+3.1415 -128 +325
+
+"abc\n" "something xyz" ""
+
+ThisSymbol 3.14.18 123abc
+
+(define x"string")
+
+(define y;some comment
+  123)
+
++
